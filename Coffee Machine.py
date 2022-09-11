2c479c4d9d50471cba2188614472a3b933a26d0f
Łukasz Świątek Brzeziński <107208001+Lukasz2506@users.noreply.github.com>
2022-09-11 11:43:37 +0200

    Create Coffee Machine.py
    This is the requirements which were compulsory for this task (100 Days Of Code, Angela Yu):
    
    Coffee Machine Program Requirements
    1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
    a. Check the user’s input to decide what to do next.
    b. The prompt should show every time action has completed, e.g. once the drink is
    dispensed. The prompt should show again to serve the next customer.
    2. Turn off the Coffee Machine by entering “off” to the prompt.
    a. For maintainers of the coffee machine, they can use “off” as the secret word to turn off
    the machine. Your code should end execution when this happens.
    3. Print report.
    a. When the user enters “report” to the prompt, a report should be generated that shows
    the current resource values. e.g.
    Water: 100ml
    Milk: 50ml
    Coffee: 76g
    Money: $2.5
    4. Check resources sufficient?
    a. When the user chooses a drink, the program should check if there are enough
    resources to make that drink.
    b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should
    not continue to make the drink but print: “Sorry there is not enough water.”
    c. The same should happen if another resource is depleted, e.g. milk or coffee.
    5. Process coins.
    a. If there are sufficient resources to make the drink selected, then the program should
    prompt the user to insert coins.
    b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
    c. Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
    pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
    6. Check transaction successful?
    a. Check that the user has inserted enough money to purchase the drink they selected.
    E.g Latte cost $2.50, but they only inserted $0.52 then after counting the coins the
    program should say “Sorry that's not enough money. Money refunded.”.
    b. But if the user has inserted enough money, then the cost of the drink gets added to the
    machine as the profit and this will be reflected the next time “report” is triggered. E.g.
    Water: 100ml
    Milk: 50ml
    Coffee: 76g
    Money: $2.5
    c. If the user has inserted too much money, the machine should offer change.
    E.g. “Here is $2.45 dollars in change.” The change should be rounded to 2 decimal
    places.
    7. Make Coffee.
    a. If the transaction is successful and there are enough resources to make the drink the
    user selected, then the ingredients to make the drink should be deducted from the
    coffee machine resources.
    E.g. report before purchasing latte:
    Water: 300ml
    Milk: 200ml
    Coffee: 100g
    Money: $0
    Report after purchasing latte:
    Water: 100ml
    Milk: 50ml
    Coffee: 76g
    Money: $2.5
    b. Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”. If
    latte was their choice of drink

---
+++ Coffee Machine.py
@@ -0,0 +1,172 @@
+# 100 Days of Code, Angela Yu
+# Coffee Machine Project
+#Student: Łukasz Świątek Brzeziński, my own workshop
+
+# TODO: 2. Check resources are sufficient to make a drink.
+
+def is_enough_sources(coffee_type):
+    '''Check is the sources level in the Coffee Machine are enough
+       to make your coffee (coffee_type: espresso, latte, cappuccino)'''
+    if resources["water"] <= MENU[coffee_type]["ingredients"]["water"]:
+        if resources["coffee"] <= MENU[coffee_type]["ingredients"]["coffee"]:
+            if resources["milk"] <= MENU[coffee_type]["ingredients"]["milk"]:
+                return False
+                print("Sorry there is not enough water, coffe, and milk.")
+            print("Sorry there is not enough water and coffe.")
+            return False
+        print("Sorry there is not enough water.")
+        return False
+
+    elif resources["coffee"] <= MENU[coffee_type]["ingredients"]["coffee"]:
+        if resources["water"] <= MENU[coffee_type]["ingredients"]["water"]:
+            if resources["milk"] <= MENU[coffee_type]["ingredients"]["milk"]:
+                print("Sorry there is not enough water, coffe, and milk.")
+                return False
+            print("Sorry there is not enough water and coffe.")
+            return False
+        print("Sorry there is not enough coffe.")
+        return False
+
+    elif resources["milk"] <= MENU[coffee_type]["ingredients"]["milk"]:
+        if resources["water"] <= MENU[coffee_type]["ingredients"]["water"]:
+            if resources["coffee"] <= MENU[coffee_type]["ingredients"]["coffee"]:
+                print("Sorry there is not enough water, coffee, and milk.")
+                return False
+            print("Sorry there is not enough water and coffe.")
+            return False
+        print("Sorry there is not enough milk.")
+        return False
+    else:
+        return True
+
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+            "milk": 0,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+profit = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+# is_enough_sources('cappuccino')
+
+
+# TODO: 1. Print the report of all coffee machine sources.
+
+def report():
+    '''Print report for all resources Coffee Machine
+       currently has.'''
+    for key in resources:
+        print(f"{key}: {resources[key]},")
+
+
+# TODO: 5. Process coins (after sources check), calculate the money.
+
+coins = {
+    "penny": 0.01,
+    "dime": 0.1,
+    "nickel": 0.05,
+    "quarter": 0.25
+}
+
+def coins_process():
+    '''Ask for the coins you put in the machine and count the money sum.'''
+    how_many_pennies = int(input("How many pennies? "))
+    how_many_dimes = int(input("How many dimes?"))
+    how_many_nickel = int(input("How many nickel? "))
+    how_many_quarter = int(input("How many quarter? "))
+    money_sum = (how_many_quarter * coins['quarter']) + (how_many_pennies * coins['penny']) + (
+            how_many_nickel * coins['nickel']) + (how_many_dimes * coins['dime'])
+    print(f"${money_sum}")
+    return money_sum
+
+
+# TODO: 6. Check is enough money to prepare the coffe. Actualise sources if coffe will be proceeded.
+
+def is_enough_money(coffee_cost, money_sum):
+    """Compare is the coffe price lower then money you putted
+       into the coffe machine."""
+    if coffee_cost == money_sum:
+        return True
+    elif coffee_cost < money_sum:
+        difference = money_sum - coffee_cost
+        print(f"Here is ${difference} dollars in change.")
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+
+# TODO: 7. Create resources actualisation:
+
+def sources_update(coffee_type, resources):
+    for key in resources:
+        resources[key] = resources[key] - MENU[coffee_type]['ingredients'][key]
+    return resources
+
+
+# TODO: 4. Create turn off machine function
+
+def machine_power(turn_on, coffee_type):
+    turn_on = True
+    if coffee_type == 'off':
+        print("Good Bye!")
+        turn_on = False
+    return turn_on
+
+# TODO: 8. Make a function which manage the Coffee Machine work
+# TODO: 3. Ask the user what to do and prompt the choice. Prompts on each step.
+
+def CoffeMachine(MENU, resources):
+
+    turn_on = True
+    coffee_type = input("What would you like? (espresso, latte, cappuccino): ").strip().lower()
+    turn_on = machine_power(turn_on, coffee_type)
+
+    while turn_on:
+
+
+        if coffee_type == "report":
+            report()
+        else:
+            enough_sources = is_enough_sources(coffee_type)
+            if enough_sources == True:
+                payment = coins_process()
+                print(payment)
+                print(coffee_type)
+                evaluation = is_enough_money(MENU[coffee_type]['cost'], payment)
+                if evaluation:
+                    resources = sources_update(coffee_type, resources)
+                    print(f"Here is your {coffee_type}. Enjoy!")
+
+        coffee_type = input("What would you like? (espresso, latte, cappuccino): ").strip().lower()
+        turn_on = machine_power(turn_on, coffee_type)
+
+CoffeMachine(MENU, resources)
